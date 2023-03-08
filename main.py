8c1049bb1b38f2769cd7d87073e64b62b38460ae
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-03-08 18:59:02 +0900

    会話で終了できるように

--- main.py
+++ main.py
@@ -5,17 +5,25 @@ from voicevox import text_to_voice
 from conf import APIKEY
 
 openai.api_key = APIKEY
+EXIT_PHRASE = 'exit'
 
 def main():
     messages = [
         {'role': 'system', 'content': 'You are a helpful assistant.'},
+        {'role': 'user', 'content': f'終了やストップなどの会話を終了する内容で話しかけられた場合は{EXIT_PHRASE}のみを返答してください。'}
     ]
-    while True:
+    exit_flag = False
+    while not exit_flag:
         text = voice_to_text()
         messages.append(
             {'role': 'user', 'content': text}
         )
         response = chat(messages)
+
+        if response == EXIT_PHRASE:
+            exit_flag = True
+            response = 'またね！'
+
         messages.append(
             {'role': 'assistant', 'content': response}
         )
